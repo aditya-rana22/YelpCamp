db28e439b3051e50598b1470eb31ed45cd30b242
Aditya Rana <123861850+aditya-rana22@users.noreply.github.com>
2025-05-29 00:50:50 +0530

    Commented out unnecessary logs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-console.log(process.env.secret);
+// console.log(process.env.secret);
 
 const express = require("express");
 const router = express.Router();
